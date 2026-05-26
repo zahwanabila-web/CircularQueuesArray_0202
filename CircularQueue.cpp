3cf9fc6199672dba3bbbbec7d04ba3a2bea5eb61
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-26 11:15:39 +0700

    commit14

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -123,7 +123,49 @@ int main()
 
     while (true)
     {
-        
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement Insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
     }
-    return 0;
+
 }
