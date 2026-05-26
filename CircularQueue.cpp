0a5cc5c6e1766df843b17359af3f1f49e6488ce9
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-26 10:24:22 +0700

    commit2

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -7,4 +7,15 @@ private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
+ public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        
+    }
 };
