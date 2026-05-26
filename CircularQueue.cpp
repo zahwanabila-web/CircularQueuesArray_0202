529b0624540dc631dded0c85bec9a1495421af4d
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-26 10:36:02 +0700

    commit4

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -24,7 +24,34 @@ private:
         // cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max -1)
+                REAR = 0;
+            else 
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] =num;
+    }
+
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
         }
     }
 };
