db0646d414d7135fad800403b50137474ee0618f
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-26 10:39:14 +0700

    commit5

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -53,5 +53,17 @@ private:
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu element
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            
+        }
     }
 };
